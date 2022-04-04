6cf92a99589e6292282097ddff0c4c9f4a372b98
Iwona Gorbacz <92158571+igorbacz@users.noreply.github.com>
2022-04-04 22:58:43 +0200

    not finished yet

---
+++ web/src/pages/productsPage/ProductsPage.tsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+// //import { ProductItem } from '../../components/item/Item';
+
+// export const ProductsPage = () => {
+//     return (
+//         <div>
+//             <ProductItem
+//                 title="butyy"
+//                 description="jakieś tam butyyyy"
+//                 url="https://img01.ztat.net/article/spp-media-p1/e6bcc8adf83c4735b1817458436fbb9a/3e2719fe1b8744b3ab9d2b1abfbf2a6c.jpg?imwidth=1800&filter=packshot"
+//                 price={25}
+//             />
+//         </div>
+//     );
+// };
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,7 +1,9 @@
 import { MainPage } from './pages/mainPage';
+// import { ProductsPage } from './pages/productsPage/ProductsPage';
 
 export enum RoutesPaths {
     MainPage = '/',
+    ProductsPage = '/products/:type',
 }
 
 export const routes = [
@@ -10,4 +12,9 @@ export const routes = [
         element: <MainPage />,
         key: 'main-page',
     },
+    // {
+    //     path: RoutesPaths.ProductsPage,
+    //     element: <ProductsPage />,
+    //     key: 'products-page',
+    // },
 ];
