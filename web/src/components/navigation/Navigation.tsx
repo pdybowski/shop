66fa1b1d08a2eaf98ab320360f61d469397e1ee3
Piotr Dybowski <pdybowski@lodasoft.com>
2022-04-04 21:24:14 +0200

    redo navigation

--- web/src/components/navigation/Navigation.tsx
+++ web/src/components/navigation/Navigation.tsx
@@ -1,141 +1,81 @@
-import React from 'react';
 import { RoutesPaths } from '../../routes';
+import { Item, NavItem } from './components';
 import './style.css';
 
-type link = { menu?: string; submenu?: string; name: string; to?: RoutesPaths; dropdown: boolean };
-
-let navigationLinks = [
+const navigationLinks: Item[] = [
     {
         name: 'Main Page',
-        to: RoutesPaths.MainPage,
-        dropdown: false,
+        link: RoutesPaths.MainPage,
     },
     {
         name: 'Sports',
-        to: RoutesPaths.Sports,
-        dropdown: true,
+        link: RoutesPaths.Sports,
+        children: [
+            {
+                name: 'Tennis',
+                link: RoutesPaths.MainPage,
+            },
+            {
+                name: 'Football',
+                link: RoutesPaths.MainPage,
+            },
+        ],
     },
     {
         name: 'Woman',
-        to: RoutesPaths.Woman,
-        dropdown: true,
+        link: RoutesPaths.Woman,
+        children: [
+            {
+                name: 'Female clothing',
+                link: RoutesPaths.MainPage,
+                children: [
+                    {
+                        name: 'Female jackets',
+                        link: RoutesPaths.MainPage,
+                    },
+                ],
+            },
+            {
+                name: 'Female shoes',
+                link: RoutesPaths.MainPage,
+            },
+        ],
     },
     {
         name: 'Man',
-        to: RoutesPaths.Man,
-        dropdown: true,
+        link: RoutesPaths.Man,
+        children: [
+            {
+                name: "Men's clothing",
+                link: RoutesPaths.MainPage,
+            },
+            {
+                name: "Men's shoes",
+                link: RoutesPaths.MainPage,
+            },
+        ],
     },
     {
         name: 'Child',
-        to: RoutesPaths.Child,
-        dropdown: true,
+        link: RoutesPaths.Child,
     },
     {
         name: 'Brands',
-        to: RoutesPaths.Brands,
-        dropdown: true,
+        link: RoutesPaths.Brands,
     },
     {
         name: 'Bestsellers',
-        to: RoutesPaths.Bestsellers,
-        dropdown: false,
-    },
-    {
-        menu: 'Sports',
-        name: 'Tennis',
-        to: RoutesPaths.MainPage,
-        dropdown: false,
-    },
-    {
-        menu: 'Sports',
-        name: 'Football',
-        to: RoutesPaths.MainPage,
-        dropdown: false,
-    },
-    {
-        menu: 'Woman',
-        name: 'Female clothing',
-        to: RoutesPaths.MainPage,
-        dropdown: true,
-    },
-    {
-        menu: 'Woman',
-        name: 'Female shoes',
-        to: RoutesPaths.MainPage,
-        dropdown: false,
-    },
-    {
-        menu: 'Man',
-        name: "Men's clothing",
-        to: RoutesPaths.MainPage,
-        dropdown: false,
-    },
-    {
-        menu: 'Man',
-        name: "Men's shoes",
-        to: RoutesPaths.MainPage,
-        dropdown: false,
-    },
-    {
-        submenu: 'Female clothing',
-        name: 'Female jackets',
-        to: RoutesPaths.MainPage,
-        dropdown: false,
+        link: RoutesPaths.Bestsellers,
     },
 ];
 
 export const Navigation = () => {
-    function chooseMainNav(link: link) {
-        if (!link.menu && !link.submenu) return link;
-    }
-
-    const NavLinksMain: any = () =>
-        navigationLinks.filter(chooseMainNav).map((link: link) => (
-            <li key={link.name}>
-                <a href={link.to}>
-                    {link.name}
-                    {link.dropdown && <span className="arrow">&#x25BC;</span>}
-                </a>
-                {link.dropdown && (
-                    <ul className="submenu">
-                        {navigationLinks
-                            .filter((value) => {
-                                return value.menu === link.name;
-                            })
-                            .map((linkSubmenu: link) => (
-                                <li key={linkSubmenu.name}>
-                                    <a href={linkSubmenu.to}>
-                                        {linkSubmenu.name}
-                                        {linkSubmenu.dropdown && (
-                                            <span className="arrow">&#x25B6;</span>
-                                        )}
-                                    </a>
-                                    {linkSubmenu.dropdown && (
-                                        <ul className="submenu-2">
-                                            {navigationLinks
-                                                .filter((value) => {
-                                                    return value.submenu === linkSubmenu.name;
-                                                })
-                                                .map((linkSubmenu2: link) => (
-                                                    <li key={linkSubmenu2.name}>
-                                                        <a href={linkSubmenu2.to}>
-                                                            {linkSubmenu2.name}
-                                                        </a>
-                                                    </li>
-                                                ))}
-                                        </ul>
-                                    )}
-                                </li>
-                            ))}
-                    </ul>
-                )}
-            </li>
-        ));
-
     return (
         <nav className="main-nav">
-            <ul>
-                <NavLinksMain />
+            <ul className="submenu">
+                {navigationLinks.map((menu: Item) => (
+                    <NavItem key={menu.name} child={menu} level={1} />
+                ))}
             </ul>
         </nav>
     );
