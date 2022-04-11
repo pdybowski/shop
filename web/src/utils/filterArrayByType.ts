620707154f3622e39ba381e869fb1869b682dd2e
Piotr Dybowski <pdybowski@lodasoft.com>
2022-04-11 12:26:17 +0200

    maintenance

--- web/src/utils/filterArrayByType.ts
+++ web/src/utils/filterArrayByType.ts
@@ -1,3 +1,7 @@
 export function filterArrayByType<A>(array: A[], property: keyof A, filter: A[keyof A]): A[] {
-    return array.filter(a => a[property] === filter);
+    return array.filter(a => {
+        const aVal = `${a[property]}`.toLowerCase();
+        const fVal = `${filter}`.toLowerCase();
+        return aVal === fVal;
+    });
 }
