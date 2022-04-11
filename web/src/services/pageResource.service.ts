69fd49f860141b04a937dbce237ce39bb1521c74
Piotr Dybowski <pdybowski@lodasoft.com>
2022-04-11 18:31:42 +0200

    page resource in ui

---
+++ web/src/services/pageResource.service.ts
@@ -0,0 +1,10 @@
+import { PageResource } from "../interfaces";
+
+export const getEnabledPageResource = (input: PageResource): PageResource => {
+    const pageResource = { ...input };
+
+    pageResource.brands = pageResource.brands.filter(b => b.isEnabled);
+    pageResource.sports = pageResource.sports.filter(s => s.isEnabled);
+
+    return pageResource;
+}
