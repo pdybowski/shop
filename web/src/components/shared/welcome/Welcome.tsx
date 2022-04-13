909e6fbcbeca0d99ebc48031f99e8af22c9ed8d3
Justyna Gładysz <jus.gladysz@gmail.com>
2022-04-13 16:00:49 +0200

    Init welcome component

---
+++ web/src/components/shared/welcome/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import Typing from '../typing/Typing';
+
+export const Welcome = () => {
+    return (
+        <div className={'welcome'}>
+            <Typing className={'welcome-text'} typedWords={['Hello!', 'Welcome to Gen sports!']} />
+        </div>
+    );
+};
