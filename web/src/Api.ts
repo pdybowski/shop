f0c94a4b3278e0d499e4358188bee658461cddac
Tomasz <przadkatomasz.alt@gmail.com>
2022-04-20 23:51:11 +0200

    fix localhost port for stage api

--- web/src/Api.ts
+++ web/src/Api.ts
@@ -2,7 +2,10 @@ import axios from 'axios';
 import { ApiMethod } from './interfaces';
 
 export class Api {
-    private readonly BASE_URL = process.env.REACT_APP_API_URL?.trim() === 'local' ? 'http://localhost:3002/api/' : 'https://shop-coders-camp.herokuapp.com/api/';
+    private readonly BASE_URL =
+        process.env.REACT_APP_API_URL?.trim() === 'local'
+            ? 'http://localhost:3001/api/'
+            : 'https://shop-coders-camp.herokuapp.com/api/';
 
     constructor() {}
 
@@ -14,13 +17,13 @@ export class Api {
         const params = {
             method: method,
             url: `${url}`,
-        }
+        };
 
         try {
-            const { data }: { data : T } = await this.instance(params); // TODO adjust to our api
+            const { data }: { data: T } = await this.instance(params); // TODO adjust to our api
             return data;
-        } catch(error: any) {
-            throw new Error(error.message)
+        } catch (error: any) {
+            throw new Error(error.message);
         }
     }
-} 
+}
