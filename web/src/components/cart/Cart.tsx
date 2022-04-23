1b0dcdb9b9f3c1d0473889dfd05c90e1693acd9a
Justyna Gładysz <jus.gladysz@gmail.com>
2022-04-23 21:46:37 +0200

    Fix Cart component styles

--- web/src/components/cart/Cart.tsx
+++ web/src/components/cart/Cart.tsx
@@ -20,34 +20,35 @@ export const Cart = () => {
     }, 0);
 
     return (
-        <div className="cart">
-            <div className="cart__list">
+        <div className='cart'>
+            <div className='cart__list'>
                 {cartContext.cart.length <= 0 && <div>No Item in the Cart!</div>}
                 <ul>
                     {cartContext.cart.map((cartItem: { product: Product; quantity: number }) => (
                         <li key={cartItem.product['_id']}>
-                            <div className="cart__item">
-                                <div className="cart__item-image">
+                            <div className='cart__item'>
+                                <div className='cart__item-image'>
                                     <img
                                         src={cartItem.product.img}
                                         style={{ height: '120px' }}
                                         alt={cartItem.product.name}
                                     />
                                 </div>
-                                <div className="cart__item-about">
-                                    <h1 className="cart__item-title">{cartItem.product.name}</h1>
-                                    <h3 className="cart__item-brand">{cartItem.product.brand}</h3>
+                                <div className='cart__item-about'>
+                                    <h1 className='cart__item-title'>{cartItem.product.name}</h1>
+                                    <h3 className='cart__item-brand'>{cartItem.product.brand}</h3>
                                 </div>
-                                <div className="cart__item-prices">
-                                    <div className="amount">{cartItem.product.price} {CURRENCY_TYPE}/pcs</div>
+                                <div className='cart__item-prices'>
+                                    <div className='amount'>{cartItem.product.price} {CURRENCY_TYPE}/pcs</div>
                                 </div>
-                                <div className="cart__item-prices-total">
-                                    <div className="amount">{CURRENCY_TYPE}{cartItem.product.price*cartItem.quantity}</div>
+                                <div className='cart__item-prices-total'>
+                                    <div
+                                        className='amount'>{CURRENCY_TYPE}{cartItem.product.price * cartItem.quantity}</div>
                                 </div>
-                                <div className="cart__item-counter">
+                                <div className='cart__item-counter'>
                                     <div>
                                         <button
-                                            className="btn"
+                                            className='btn'
                                             onClick={() =>
                                                 cartContext.addProductToCart(cartItem.product)
                                             }
@@ -55,13 +56,13 @@ export const Cart = () => {
                                             +
                                         </button>
                                     </div>
-                                    <div className="count">{cartItem.quantity}</div>
+                                    <div className='count'>{cartItem.quantity}</div>
                                     <div>
                                         <button
-                                            className="btn"
+                                            className='btn'
                                             onClick={() =>
                                                 cartContext.removeProductFromCart(
-                                                    cartItem.product['_id']
+                                                    cartItem.product['_id'],
                                                 )
                                             }
                                         >
@@ -75,15 +76,17 @@ export const Cart = () => {
                 </ul>
             </div>
             {cartContext.cart.length > 0 && (
-                <div className="cart__checkout">
-                    <div className="cart__checkout-total">
+                <div className='cart__checkout'>
+                    <div className='cart__checkout-total'>
                         <div>
-                            <div className="cart__checkout-total-subtotal">Sub-Total</div>
-                            <div className="cart__checkout-total-items">{cartItemNumber} items</div>
+                            <div className='cart__checkout-total-subtotal'>Sub-Total</div>
+                            <div className='cart__checkout-total-items'>{cartItemNumber} items</div>
                         </div>
-                        <div className="cart__checkout-total-amount">{CURRENCY_TYPE}{subTotal}</div>
+                        <div className='cart__checkout-total-amount'>{CURRENCY_TYPE}{subTotal}</div>
                     </div>
-                    <button className="cart__checkout-button">Go to payment</button>
+                    <div>
+                        <button className='cart__checkout-button'>Go to payment</button>
+                    </div>
                 </div>
             )}
         </div>
