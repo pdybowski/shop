64addfa42c7f0c9bb3916e2e5a7220ea3f861272
Justyna Gładysz <jus.gladysz@gmail.com>
2022-04-23 17:48:55 +0200

    Refactor Products Page

--- web/src/components/pages/productsPage/ProductsPage.tsx
+++ web/src/components/pages/productsPage/ProductsPage.tsx
@@ -21,6 +21,10 @@ export const ProductsPage = (): JSX.Element => {
     const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
     const [searchParams] = useSearchParams();
 
+    const [nameSearch, setNameSearch] = useState('');
+    const [minPriceSearch, setMinPriceSearch] = useState(0);
+    // const [maxPriceSearch, setMaxPriceSearch] = useState(0);
+
     const sportType = searchParams.get('sportType') as SportType;
     const productType = searchParams.get('productType') as ProductType;
     const productCategory = searchParams.get('productCategory') as ProductCategory;
@@ -33,15 +37,65 @@ export const ProductsPage = (): JSX.Element => {
 
     const searchProduct = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { value } = e.target;
+        setNameSearch(value);
+    };
 
-        const results: Product[] = productsFilteredByType.filter(
+    const searchProductByMinPrice = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setMinPriceSearch(parseInt(value));
+    };
+
+    // const searchProductByMaxPrice = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const { value } = e.target;
+    //     setMaxPriceSearch(parseInt(value));
+    // };
+
+    useEffect(() => {
+        if (nameSearch === '' && minPriceSearch === 0) {
+            setTotalPages(Math.ceil(productsFilteredByType.length / itemsPerPage));
+            setFilteredProducts(productsFilteredByType);
+            return;
+        }
+
+        const results: Product[] = filteredProducts.filter(
             (product) =>
-                product.name.toLowerCase().includes(value.toLowerCase()) ||
-                product.description.toLowerCase().includes(value.toLowerCase()),
+                product.name.toLowerCase().includes(nameSearch.toLowerCase()) ||
+                product.description.toLowerCase().includes(nameSearch.toLowerCase()),
         );
         setTotalPages(Math.ceil(results.length / itemsPerPage));
         setFilteredProducts(results);
-    };
+    }, [nameSearch]);
+
+    useEffect(() => {
+        if (nameSearch === '' && minPriceSearch === 0) {
+            setTotalPages(Math.ceil(productsFilteredByType.length / itemsPerPage));
+            setFilteredProducts(productsFilteredByType);
+            return;
+        }
+
+        const results: Product[] = filteredProducts.filter(
+            (product) =>
+                product.price >= minPriceSearch,
+        );
+        setTotalPages(Math.ceil(results.length / itemsPerPage));
+        setFilteredProducts(results);
+    }, [minPriceSearch]);
+
+    // useEffect(() => {
+    //     if (nameSearch === '' && minPriceSearch === 0) {
+    //         setTotalPages(Math.ceil(productsFilteredByType.length / itemsPerPage));
+    //         setFilteredProducts(productsFilteredByType);
+    //         return;
+    //     }
+    //
+    //     const results: Product[] = filteredProducts.filter(
+    //         (product) =>
+    //             product.price <= maxPriceSearch,
+    //     );
+    //     setTotalPages(Math.ceil(results.length / itemsPerPage));
+    //     setFilteredProducts(results);
+    // }, [maxPriceSearch]);
+
 
     useMemo(() => {
         if (!productCategory) return <Navigate replace to='/' />;
@@ -77,6 +131,8 @@ export const ProductsPage = (): JSX.Element => {
         <div className='products__page'>
             <h2 className='products__page__title'>{header}</h2>
             <SearchInput onSearch={searchProduct} />
+            <SearchInput onSearch={searchProductByMinPrice} />
+            {/*<SearchInput onSearch={searchProductByMaxPrice} />*/}
             <div className='products__page__items'>
                 {filteredProducts.map((item) => {
                     return <ProductItem key={item._id} {...item} />;
