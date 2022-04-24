434647834b9acdc1c4ef197009067ff7f0f50848
Tomasz <przadkatomasz.alt@gmail.com>
2022-04-24 02:21:27 +0200

    make WebkitLineClamp inline

--- web/src/components/shared/ellipsisWrapper/EllipsisWrapper.tsx
+++ web/src/components/shared/ellipsisWrapper/EllipsisWrapper.tsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
 import './style.css';
 
 interface EllipsisWrapperProps {
     children?: JSX.Element | JSX.Element[] | string;
+    textLength?: number;
 }
 
-export const EllipsisWrapper = ({ children }: EllipsisWrapperProps) => {
-    return <div className="block-with-text">{children}</div>;
+export const EllipsisWrapper = ({ children, textLength = 5 }: EllipsisWrapperProps) => {
+
+    return (
+        <div className="block-with-text" style={{ WebkitLineClamp: textLength }}>
+            {children}
+        </div>
+    );
 };
