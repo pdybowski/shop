94df68ff26b9483cf833c2417da088bb2266cae5
Piotr Dybowski <pdybowski@lodasoft.com>
2022-05-07 00:22:18 +0200

    popup + btn fix

--- web/src/components/shared/button/Button.tsx
+++ web/src/components/shared/button/Button.tsx
@@ -1,14 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import { ButtonProps } from './interfaces';
 import './style.css';
 
-export const Button = ({ label, mode, type, ...args }: ButtonProps) => {
-    return (
-        <div>
-            <button className={`button button--${mode}`} type={type} {...args}>
-                {label}
-            </button>
-        </div>
-    );
-};
+export const Button = ({ children, mode, type, ...args }: ButtonProps) => (
+    <button className={`button button--${mode}`} type={type} {...args}>
+        {children}
+    </button>
+);
---
+++ web/src/components/shared/popup/Popup.tsx
@@ -0,0 +1,75 @@
+import { useEffect } from 'react';
+import { IoMdClose } from 'react-icons/io';
+import { Button } from '../button/Button';
+import { ButtonMode } from '../button/interfaces';
+
+import './style.css';
+
+interface Props {
+    headerText?: string;
+    saveBtnText?: string;
+    isDisplayed: boolean;
+    children: JSX.Element;
+    handleClose: () => void;
+    handleSubmit?: () => void;
+}
+
+export const Popup = ({
+    headerText,
+    saveBtnText,
+    isDisplayed,
+    children,
+    handleClose,
+    handleSubmit,
+}: Props) => {
+    useEffect(() => {
+        document.body.style.overflow = isDisplayed ? 'hidden' : 'auto';
+    }, [isDisplayed]);
+
+    if (!isDisplayed) return <></>;
+
+    const handleOverlayClick = (e: any) => {
+        e.preventDefault();
+
+        console.log([...e.target.classList]);
+        const isOverlay = [...e.target.classList].indexOf('popup__overlay') > -1;
+        if (isOverlay) {
+            handleClose();
+        }
+    };
+
+    return (
+        <div className="popup__overlay" onClick={handleOverlayClick}>
+            <div className="popup">
+                {headerText && <div className="popup__header">{headerText}</div>}
+
+                {<div className="popup__body">{children}</div>}
+
+                <div className="popup__footer">
+                    <Button
+                        type="reset"
+                        mode={ButtonMode.CANCEL}
+                        onClick={handleClose}
+                        style={{ margin: '0px' }}
+                    >
+                        Cancel
+                    </Button>
+                    {saveBtnText && (
+                        <Button
+                            type="submit"
+                            mode={ButtonMode.PRIMARY}
+                            style={{ margin: '0px' }}
+                            onClick={handleSubmit}
+                        >
+                            {saveBtnText}
+                        </Button>
+                    )}
+                </div>
+
+                <div className="popup__close cursor--pointer" onClick={handleClose}>
+                    <IoMdClose />
+                </div>
+            </div>
+        </div>
+    );
+};
