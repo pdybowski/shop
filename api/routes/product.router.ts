51047e68489e45a48714c0670fa9f5aebfe7b542
Piotr Dybowski <pdybowski@lodasoft.com>
2022-05-09 16:22:14 +0200

    sell count to product api model

--- api/routes/product.router.ts
+++ api/routes/product.router.ts
@@ -1,14 +1,20 @@
 import { Router } from 'express'
-import { getAllProductsHandler, getSingleProductHandler, createProductHandler, updateSingleProductHandler, deleteProductHandler} from '../controllers/product.controller'
+import { getAllProductsHandler, getSingleProductHandler, createProductHandler, buyProductsHandler, updateSingleProductHandler, deleteProductHandler} from '../controllers/product.controller'
 
 const routes = Router();
 
 // get all products WORKING
 routes.get('/', getAllProductsHandler)
+
 // get single product WORKING
 routes.get('/:id', getSingleProductHandler)
+
 // create product WORKING
 routes.post('/', createProductHandler)
+
+// buy products
+routes.put('/buy', buyProductsHandler)
+
 
 
 
