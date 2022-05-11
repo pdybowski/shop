65676636977cf824f3e8158fd4c88ed1531a7ac3
Piotr Dybowski <pdybowski@lodasoft.com>
2022-05-11 14:49:37 +0200

    some style fixes

--- web/src/components/shared/footer/Footer.tsx
+++ web/src/components/shared/footer/Footer.tsx
@@ -16,55 +16,63 @@ export const Footer: React.FC = () => {
         } else {
             return <span>2022 - {currentYear} </span>;
         }
-
     }
 
     return (
         <div className={'footer-container'}>
             <div className={'footer'}>
                 <div className={'footer__row'}>
-                    <div className='footer__row1__social'>
-                        <a className='footer__social-item' href='#'>
-                            <img alt='twitter' src={twitter} /></a>
-                        <a className='footer__social-item' href='#'>
-                            <img alt='facebook' src={facebook} /></a>
-                        <a className='footer__social-item ' href='#'>
-                            <img alt='instagram' src={instagram} /></a>
-                        <a className='footer__social-item ' href='#'>
-                            <img alt='pinterest' src={pinterest} /></a>
+                    <div className="footer__row1__social">
+                        <a className="footer__social-item twitter" href="#">
+                            <img alt="twitter" src={twitter} />
+                        </a>
+                        <a className="footer__social-item" href="#">
+                            <img alt="facebook" src={facebook} />
+                        </a>
+                        <a className="footer__social-item " href="#">
+                            <img alt="instagram" src={instagram} />
+                        </a>
+                        <a className="footer__social-item " href="#">
+                            <img alt="pinterest" src={pinterest} />
+                        </a>
                     </div>
-                    <div className='footer__row1__policy'>
-                        <a className='footer__policy-item' href='#'>Returns Policy</a>
-                        <a className='footer__policy-item' href='#'>Privacy Policy</a>
-                        <a className='footer__policy-item' href='#'>Privacy Policy</a>
+                    <div className="footer__row1__policy">
+                        <a className="footer__policy-item" href="#">
+                            Returns Policy
+                        </a>
+                        <a className="footer__policy-item" href="#">
+                            Privacy Policy
+                        </a>
                     </div>
-                    <div className='footer__row1__currency-language'>
+                    <div className="footer__row1__currency-language">
                         <div>
-                            <label className='footer__label'>Currency</label>
-                            <button className='footer__button' type='button'>
+                            <label className="footer__label">Currency</label>
+                            <button className="footer__button" type="button">
                                 USD
-                                <span className='dropdown-toggle-icon'></span>
+                                <span className="dropdown-toggle-icon"></span>
                             </button>
                         </div>
                         <div>
-                            <label className='footer__label'>Language</label>
-                            <button className='footer__button' type='button'>
+                            <label className="footer__label">Language</label>
+                            <button className="footer__button" type="button">
                                 ENGLISH
-                                <span className='dropdown-toggle-icon'></span>
+                                <span className="dropdown-toggle-icon"></span>
                             </button>
                         </div>
                     </div>
                 </div>
                 <div className={'footer__row'}>
-                    <div className='footer__row2__rights'>
-                        <p className='rights'>&copy;
+                    <div className="footer__row2__rights">
+                        <p className="rights">
+                            &copy;
                             <span>{showRightsYear()}</span>
-                            <span>All rights reserved</span>
+                            <span> - Sport Gen</span>
                         </p>
                     </div>
-                    <div className='footer__row2__payment'>
-                        <a className='footer__payment-item' href='#'>
-                            <img alt='payment' src={payment} /></a>
+                    <div className="footer__row2__payment">
+                        <a className="footer__payment-item" href="#">
+                            <img alt="payment" src={payment} />
+                        </a>
                     </div>
                 </div>
             </div>
