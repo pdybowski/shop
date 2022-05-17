efa50180c83fc0bf8ffd3bf3af61b0ef2400fb64
Tomasz <przadkatomasz.alt@gmail.com>
2022-05-18 00:11:27 +0200

    add user model

---
+++ api/models/user.model.ts
@@ -0,0 +1,61 @@
+import {
+    DocumentType,
+    getModelForClass,
+    index,
+    modelOptions,
+    pre,
+    prop,
+  } from '@typegoose/typegoose';
+  import bcrypt from 'bcrypt';
+  
+  // @index({ email: 1 })
+  @pre<User>('save', async function () {
+    // Hash password if the password is new or was updated
+    if (!this.isModified('password')) return;
+  
+    // Hash password with costFactor of 12
+    this.password = await bcrypt.hash(this.password, 12);
+  })
+  @modelOptions({
+    schemaOptions: {
+      // Add createdAt and updatedAt fields
+      timestamps: true,
+    },
+  })
+  
+
+  export enum Role {
+      user = 'user',
+      admin = 'admin'
+  }
+
+  // Export the User class to be used as TypeScript type
+  export class User {
+    @prop({required:true})
+    firstName!: string;
+
+    @prop({required:true})
+    lastName!: string;
+  
+    @prop({ unique: true, required: true })
+    email!: string;
+  
+    @prop({ required: true, minlength: 8, maxLength: 32, select: false })
+    password!: string;
+  
+    @prop({ type: String, enum: Role, default: Role.user })
+    role: Role;
+
+    // @prop()
+  
+    // Instance method to check if passwords match
+    async comparePasswords(hashedPassword: string, candidatePassword: string) {
+      return await bcrypt.compare(candidatePassword, hashedPassword);
+    }
+  }
+  
+  // Create the user model from the User class
+  const userModel = getModelForClass(User);
+  export default userModel;
+  
+  
