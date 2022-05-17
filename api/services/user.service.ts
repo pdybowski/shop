096bcbb01ad94201e8a7f6d125dc2b51784a1ca0
Tomasz <przadkatomasz.alt@gmail.com>
2022-05-18 00:15:43 +0200

    add user service

---
+++ api/services/user.service.ts
@@ -0,0 +1,51 @@
+import { omit, get } from 'lodash';
+import { FilterQuery, QueryOptions } from 'mongoose';
+import config from 'config';
+import userModel, { User } from '../models/user.model';
+import { excludedFields } from '../controllers/auth.controller';
+import { signJwt } from '../helpers/jwt';
+import { DocumentType } from '@typegoose/typegoose';
+
+// CreateUser service
+export const createUser = async (input: Partial<User>) => {
+
+  console.log("CreateUser service")
+  const user = await userModel.create(input);
+  return omit(user.toJSON(), excludedFields);
+};
+
+// Find User by Id
+export const findUserById = async (id: string) => {
+  const user = await userModel.findById(id).lean();
+  return omit(user, excludedFields);
+};
+
+// Find All users
+export const findAllUsers = async () => {
+  return await userModel.find();
+};
+
+// Find one user by any fields
+export const findUser = async (
+  query: FilterQuery<User>,
+  options: QueryOptions = {}
+) => {
+  return await userModel.findOne(query, {}, options).select('+password');
+};
+
+// Sign Token
+export const signToken = async (user: DocumentType<User>) => {
+  // Sign the access token
+  const access_token = signJwt(
+    { sub: user._id },
+    {
+      expiresIn: `${config.get<number>('accessTokenExpiresIn')}m`,
+    }
+  );
+
+ 
+
+  // Return access token
+  return { access_token };
+};
+
