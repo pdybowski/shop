ba77163b16c39f84a1f6d367d44d606cf5a123a1
Tomasz <przadkatomasz.alt@gmail.com>
2022-05-18 00:15:26 +0200

    add jwt signing, update middleware

--- api/middleware/validateResource.ts
+++ api/middleware/validateResource.ts
@@ -1,5 +1,5 @@
 import {Request, Response, NextFunction} from 'express'
-import {AnyZodObject} from 'zod'
+import {AnyZodObject, ZodError} from 'zod'
 
 const validate = (schema: AnyZodObject) => (req: Request, res: Response, next: NextFunction) => {
 
@@ -9,9 +9,15 @@ const validate = (schema: AnyZodObject) => (req: Request, res: Response, next: N
             query: req.query,
             params: req.params
         })
-    } catch (e: any) {
-        return res.status(400).send(e.errors)
-    }
+    } catch (err: any) {
+        if (err instanceof ZodError) {
+            return res.status(400).json({
+              status: 'fail',
+              error: err.errors,
+            });
+          }
+          next(err);
+}
 }
 
 export default validate;
