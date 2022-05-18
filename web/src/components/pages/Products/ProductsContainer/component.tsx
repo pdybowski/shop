6a2e2853509252725adf400f92af2d2d6b86ff45
Justyna Gładysz <jus.gladysz@gmail.com>
2022-05-18 22:50:55 +0200

    Add sizes checkboxes

--- web/src/components/pages/Products/ProductsContainer/component.tsx
+++ web/src/components/pages/Products/ProductsContainer/component.tsx
@@ -40,6 +40,28 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
     const [checkedSizeXL, setCheckedSizeXL] = useState(false);
     const [checkedSizeXXL, setCheckedSizeXXL] = useState(false);
 
+    const [checkedSize28, setCheckedSize28] = useState(false);
+    const [checkedSize29, setCheckedSize29] = useState(false);
+    const [checkedSize30, setCheckedSize30] = useState(false);
+    const [checkedSize31, setCheckedSize31] = useState(false);
+    const [checkedSize32, setCheckedSize32] = useState(false);
+    const [checkedSize33, setCheckedSize33] = useState(false);
+    const [checkedSize34, setCheckedSize34] = useState(false);
+    const [checkedSize35, setCheckedSize35] = useState(false);
+    const [checkedSize36, setCheckedSize36] = useState(false);
+    const [checkedSize37, setCheckedSize37] = useState(false);
+    const [checkedSize38, setCheckedSize38] = useState(false);
+    const [checkedSize39, setCheckedSize39] = useState(false);
+    const [checkedSize40, setCheckedSize40] = useState(false);
+    const [checkedSize41, setCheckedSize41] = useState(false);
+    const [checkedSize42, setCheckedSize42] = useState(false);
+    const [checkedSize43, setCheckedSize43] = useState(false);
+    const [checkedSize44, setCheckedSize44] = useState(false);
+    const [checkedSize45, setCheckedSize45] = useState(false);
+    const [checkedSize46, setCheckedSize46] = useState(false);
+    const [checkedSize47, setCheckedSize47] = useState(false);
+    const [checkedSize48, setCheckedSize48] = useState(false);
+
     const handleChangeSizeXXS = () => {
         setCheckedSizeXXS(!checkedSizeXXS);
     };
@@ -68,6 +90,90 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
         setCheckedSizeXXL(!checkedSizeXXL);
     };
 
+    const handleChangeSize28 = () => {
+        setCheckedSize28(!checkedSize28);
+    };
+
+    const handleChangeSize29 = () => {
+        setCheckedSize29(!checkedSize29);
+    };
+
+    const handleChangeSize30 = () => {
+        setCheckedSize30(!checkedSize30);
+    };
+
+    const handleChangeSize31 = () => {
+        setCheckedSize31(!checkedSize31);
+    };
+
+    const handleChangeSize32 = () => {
+        setCheckedSize32(!checkedSize32);
+    };
+
+    const handleChangeSize33 = () => {
+        setCheckedSize33(!checkedSize33);
+    };
+
+    const handleChangeSize34 = () => {
+        setCheckedSize34(!checkedSize34);
+    };
+
+    const handleChangeSize35 = () => {
+        setCheckedSize35(!checkedSize35);
+    };
+
+    const handleChangeSize36 = () => {
+        setCheckedSize36(!checkedSize36);
+    };
+
+    const handleChangeSize37 = () => {
+        setCheckedSize37(!checkedSize37);
+    };
+
+    const handleChangeSize38 = () => {
+        setCheckedSize38(!checkedSize38);
+    };
+
+    const handleChangeSize39 = () => {
+        setCheckedSize39(!checkedSize39);
+    };
+
+    const handleChangeSize40 = () => {
+        setCheckedSize40(!checkedSize40);
+    };
+
+    const handleChangeSize41 = () => {
+        setCheckedSize41(!checkedSize41);
+    };
+
+    const handleChangeSize42 = () => {
+        setCheckedSize42(!checkedSize42);
+    };
+
+    const handleChangeSize43 = () => {
+        setCheckedSize43(!checkedSize43);
+    };
+
+    const handleChangeSize44 = () => {
+        setCheckedSize44(!checkedSize44);
+    };
+
+    const handleChangeSize45 = () => {
+        setCheckedSize45(!checkedSize45);
+    };
+
+    const handleChangeSize46 = () => {
+        setCheckedSize46(!checkedSize46);
+    };
+
+    const handleChangeSize47 = () => {
+        setCheckedSize47(!checkedSize47);
+    };
+
+    const handleChangeSize48 = () => {
+        setCheckedSize48(!checkedSize48);
+    };
+
     const searchProductByName = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { value } = e.target;
         setNameSearch(value);
@@ -84,12 +190,12 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
     };
 
     const addSizeToSearch = (size: tSizes | tShirtSizes) => {
-        setSizeSearchArr([...sizeSearchArr, size])
-    }
+        setSizeSearchArr([...sizeSearchArr, size]);
+    };
 
     const removeSizeFromSearch = (size: tSizes | tShirtSizes) => {
         setSizeSearchArr(sizeSearchArr.filter(item => item !== size));
-    }
+    };
 
     const searchProductBySize = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { checked, value } = e.target;
@@ -124,7 +230,7 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
 
         const filterProductsBySize = () => {
             results = results.filter(item1 =>
-                !!sizeSearchArr.find(item2 => item1.size === item2)
+                !!sizeSearchArr.find(item2 => item1.size === item2),
             );
         };
 
@@ -179,7 +285,11 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
         setNameSearch('');
     }
 
-    const SizeCheckbox = ( { size, value, onChange }: { size: tSizes | tShirtSizes, value: boolean, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void },  ) => {
+    const SizeCheckbox = ({
+                              size,
+                              value,
+                              onChange,
+                          }: { size: tSizes | tShirtSizes, value: boolean, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void }) => {
         return (
             <li className='size__checkbox'>
                 <label className='size__label cursor--pointer'>
@@ -195,6 +305,7 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
 
     return (
         <div className='products__container'>
+            <p>{productsToDisplay}</p>
             <div className='products__container--left'>
                 <div className='products__search'>
                     <SearchInput onSearch={searchProductByName} onClear={clearSearchByName} />
@@ -224,13 +335,38 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
                     {sizeDropdown && (
                         <div className='products__filter__dropdown'>
                             <ul className='products__filter__dropdown__list'>
-                                <SizeCheckbox size='XXS' value={checkedSizeXXS} onChange={handleChangeSizeXXS}/>
-                                <SizeCheckbox size='XS' value={checkedSizeXS} onChange={handleChangeSizeXS}/>
-                                <SizeCheckbox size='S' value={checkedSizeS} onChange={handleChangeSizeS}/>
-                                <SizeCheckbox size='M' value={checkedSizeM} onChange={handleChangeSizeM}/>
-                                <SizeCheckbox size='L' value={checkedSizeL} onChange={handleChangeSizeL}/>
-                                <SizeCheckbox size='XL' value={checkedSizeXL} onChange={handleChangeSizeXL}/>
-                                <SizeCheckbox size='XXL' value={checkedSizeXXL} onChange={handleChangeSizeXXL}/>
+                                <div>
+                                    <SizeCheckbox size='XXS' value={checkedSizeXXS} onChange={handleChangeSizeXXS} />
+                                    <SizeCheckbox size='XS' value={checkedSizeXS} onChange={handleChangeSizeXS} />
+                                    <SizeCheckbox size='S' value={checkedSizeS} onChange={handleChangeSizeS} />
+                                    <SizeCheckbox size='M' value={checkedSizeM} onChange={handleChangeSizeM} />
+                                    <SizeCheckbox size='L' value={checkedSizeL} onChange={handleChangeSizeL} />
+                                    <SizeCheckbox size='XL' value={checkedSizeXL} onChange={handleChangeSizeXL} />
+                                    <SizeCheckbox size='XXL' value={checkedSizeXXL} onChange={handleChangeSizeXXL} />
+                                </div>
+                                <div>
+                                    <SizeCheckbox size='28' value={checkedSize28} onChange={handleChangeSize28} />
+                                    <SizeCheckbox size='29' value={checkedSize29} onChange={handleChangeSize29} />
+                                    <SizeCheckbox size='30' value={checkedSize30} onChange={handleChangeSize30} />
+                                    <SizeCheckbox size='31' value={checkedSize31} onChange={handleChangeSize31} />
+                                    <SizeCheckbox size='32' value={checkedSize32} onChange={handleChangeSize32} />
+                                    <SizeCheckbox size='33' value={checkedSize33} onChange={handleChangeSize33} />
+                                    <SizeCheckbox size='34' value={checkedSize34} onChange={handleChangeSize34} />
+                                    <SizeCheckbox size='35' value={checkedSize35} onChange={handleChangeSize35} />
+                                    <SizeCheckbox size='36' value={checkedSize36} onChange={handleChangeSize36} />
+                                    <SizeCheckbox size='37' value={checkedSize37} onChange={handleChangeSize37} />
+                                    <SizeCheckbox size='38' value={checkedSize38} onChange={handleChangeSize38} />
+                                    <SizeCheckbox size='39' value={checkedSize39} onChange={handleChangeSize39} />
+                                    <SizeCheckbox size='40' value={checkedSize40} onChange={handleChangeSize40} />
+                                    <SizeCheckbox size='41' value={checkedSize41} onChange={handleChangeSize41} />
+                                    <SizeCheckbox size='42' value={checkedSize42} onChange={handleChangeSize42} />
+                                    <SizeCheckbox size='43' value={checkedSize43} onChange={handleChangeSize43} />
+                                    <SizeCheckbox size='44' value={checkedSize44} onChange={handleChangeSize44} />
+                                    <SizeCheckbox size='45' value={checkedSize45} onChange={handleChangeSize45} />
+                                    <SizeCheckbox size='46' value={checkedSize46} onChange={handleChangeSize46} />
+                                    <SizeCheckbox size='47' value={checkedSize47} onChange={handleChangeSize47} />
+                                    <SizeCheckbox size='48' value={checkedSize48} onChange={handleChangeSize48} />
+                                </div>
                             </ul>
                         </div>
                     )}
