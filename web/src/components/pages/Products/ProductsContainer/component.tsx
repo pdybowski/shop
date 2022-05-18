9d240fe887f125aa3e3b5976487a17ec07fe062b
Justyna Gładysz <jus.gladysz@gmail.com>
2022-05-18 15:38:53 +0200

    Fix sizes filtration

--- web/src/components/pages/Products/ProductsContainer/component.tsx
+++ web/src/components/pages/Products/ProductsContainer/component.tsx
@@ -1,22 +1,11 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { Navigate, useSearchParams } from 'react-router-dom';
 
-import { ProductItem, SearchInput, Pagination, DownArrow, UpArrow } from '../../../shared';
+import { DownArrow, Pagination, ProductItem, SearchInput, UpArrow } from '../../../shared';
 
 import { Product, ProductCategory, tShirtSizes, tSizes } from '../../../../models';
 
 import './style.css';
-
-const SizeCheckbox = ({ size }: { size: tSizes | tShirtSizes }) => {
-    return (
-        <li className="size__checkbox">
-            <label className="size__label cursor--pointer" htmlFor={`size${size}`}>
-                <input type="checkbox" id={`size${size}`} style={{ marginRight: '7px' }} />
-                {size}
-            </label>
-        </li>
-    );
-};
 
 interface Props {
     productsToDisplay: Product[];
@@ -39,8 +28,45 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
     const [nameSearch, setNameSearch] = useState('');
     const [minPriceSearch, setMinPriceSearch] = useState('');
     const [maxPriceSearch, setMaxPriceSearch] = useState('');
+    const [sizeSearchArr, setSizeSearchArr] = useState<(tSizes | tShirtSizes)[]>([]);
 
     const productCategory = searchParams.get('productCategory') as ProductCategory;
+
+    const [checkedSizeXXS, setCheckedSizeXXS] = useState(false);
+    const [checkedSizeXS, setCheckedSizeXS] = useState(false);
+    const [checkedSizeS, setCheckedSizeS] = useState(false);
+    const [checkedSizeM, setCheckedSizeM] = useState(false);
+    const [checkedSizeL, setCheckedSizeL] = useState(false);
+    const [checkedSizeXL, setCheckedSizeXL] = useState(false);
+    const [checkedSizeXXL, setCheckedSizeXXL] = useState(false);
+
+    const handleChangeSizeXXS = () => {
+        setCheckedSizeXXS(!checkedSizeXXS);
+    };
+
+    const handleChangeSizeXS = () => {
+        setCheckedSizeXS(!checkedSizeXS);
+    };
+
+    const handleChangeSizeS = () => {
+        setCheckedSizeS(!checkedSizeS);
+    };
+
+    const handleChangeSizeM = () => {
+        setCheckedSizeM(!checkedSizeM);
+    };
+
+    const handleChangeSizeL = () => {
+        setCheckedSizeL(!checkedSizeL);
+    };
+
+    const handleChangeSizeXL = () => {
+        setCheckedSizeXL(!checkedSizeXL);
+    };
+
+    const handleChangeSizeXXL = () => {
+        setCheckedSizeXXL(!checkedSizeXXL);
+    };
 
     const searchProductByName = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { value } = e.target;
@@ -57,6 +83,25 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
         setMaxPriceSearch(value);
     };
 
+    const addSizeToSearch = (size: tSizes | tShirtSizes) => {
+        setSizeSearchArr([...sizeSearchArr, size])
+    }
+
+    const removeSizeFromSearch = (size: tSizes | tShirtSizes) => {
+        setSizeSearchArr(sizeSearchArr.filter(item => item !== size));
+    }
+
+    const searchProductBySize = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { checked, value } = e.target;
+        if (checked) {
+            // @ts-ignore
+            addSizeToSearch(value);
+        } else {
+            // @ts-ignore
+            removeSizeFromSearch(value);
+        }
+    };
+
     useEffect(() => {
         let results: Product[] = productsToDisplay;
 
@@ -65,7 +110,7 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
             results = results.filter(
                 (product) =>
                     product.name.toLowerCase().includes(nameSearch.toLowerCase()) ||
-                    product.description?.toLowerCase().includes(nameSearch.toLowerCase())
+                    product.description?.toLowerCase().includes(nameSearch.toLowerCase()),
             );
         };
 
@@ -75,6 +120,12 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
 
         const filterProductsByMaxPrice = () => {
             results = results.filter((product) => product.price <= parseInt(maxPriceSearch));
+        };
+
+        const filterProductsBySize = () => {
+            results = results.filter(item1 =>
+                !!sizeSearchArr.find(item2 => item1.size === item2)
+            );
         };
 
         if (nameSearch !== '') {
@@ -89,16 +140,20 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
             filterProductsByMaxPrice();
         }
 
+        if (sizeSearchArr.length !== 0) {
+            filterProductsBySize();
+        }
+
         setFilteredProducts(results);
 
         let pageProducts = results.slice((page - 1) * itemsOnPage, page * itemsOnPage);
         setTotalPages(Math.ceil(results.length / itemsOnPage));
         setProductsForPage(pageProducts);
         setPage(1);
-    }, [nameSearch, minPriceSearch, maxPriceSearch]);
+    }, [nameSearch, minPriceSearch, maxPriceSearch, sizeSearchArr]);
 
     useMemo(() => {
-        if (!productCategory) return <Navigate replace to="/" />;
+        if (!productCategory) return <Navigate replace to='/' />;
     }, []);
 
     useEffect(() => {
@@ -120,89 +175,106 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
         setSizeDropdown(!sizeDropdown);
     }
 
-    function clearSearch() {
+    function clearSearchByName() {
         setNameSearch('');
     }
 
+    const SizeCheckbox = ( { size, value, onChange }: { size: tSizes | tShirtSizes, value: boolean, onChange: (e: React.ChangeEvent<HTMLInputElement>) => void },  ) => {
+        return (
+            <li className='size__checkbox'>
+                <label className='size__label cursor--pointer'>
+                    <input type='checkbox' checked={value} onChange={(e) => {
+                        searchProductBySize(e);
+                        onChange(e);
+                    }} style={{ marginRight: '7px' }} value={size} />
+                    {size}
+                </label>
+            </li>
+        );
+    };
+
     return (
-        <div className="products__container">
-            <div className="products__container--left">
-                <div className="products__search">
-                    <SearchInput onSearch={searchProductByName} onClear={clearSearch} />
+        <div className='products__container'>
+            <div className='products__container--left'>
+                <div className='products__search'>
+                    <SearchInput onSearch={searchProductByName} onClear={clearSearchByName} />
                 </div>
-                <div className="products__filter">
+                <div className='products__filter'>
                     <button
-                        type="button"
-                        className="products__filter__btn"
+                        type='button'
+                        className='products__filter__btn'
                         onClick={toggleSizeDropdown}
                     >
                         {sizeDropdown ? (
-                            <div className="products__filter__dropdown__name">
+                            <div className='products__filter__dropdown__name'>
                                 <p>Size</p>
-                                <p className="products__filter__arrow">
+                                <p className='products__filter__arrow'>
                                     <UpArrow />
                                 </p>
                             </div>
                         ) : (
-                            <div className="products__filter__name">
+                            <div className='products__filter__name'>
                                 <p>Size</p>
-                                <p className="products__filter__arrow">
+                                <p className='products__filter__arrow'>
                                     <DownArrow />
                                 </p>
                             </div>
                         )}
                     </button>
                     {sizeDropdown && (
-                        <div className="products__filter__dropdown">
-                            <ul className="products__filter__dropdown__list">
-                                <SizeCheckbox size="S" />
-                                <SizeCheckbox size="M" />
-                                <SizeCheckbox size="L" />
-                                <SizeCheckbox size="XL" />
+                        <div className='products__filter__dropdown'>
+                            <ul className='products__filter__dropdown__list'>
+                                <SizeCheckbox size='XXS' value={checkedSizeXXS} onChange={handleChangeSizeXXS}/>
+                                <SizeCheckbox size='XS' value={checkedSizeXS} onChange={handleChangeSizeXS}/>
+                                <SizeCheckbox size='S' value={checkedSizeS} onChange={handleChangeSizeS}/>
+                                <SizeCheckbox size='M' value={checkedSizeM} onChange={handleChangeSizeM}/>
+                                <SizeCheckbox size='L' value={checkedSizeL} onChange={handleChangeSizeL}/>
+                                <SizeCheckbox size='XL' value={checkedSizeXL} onChange={handleChangeSizeXL}/>
+                                <SizeCheckbox size='XXL' value={checkedSizeXXL} onChange={handleChangeSizeXXL}/>
                             </ul>
                         </div>
                     )}
                 </div>
-                <div className="products__filter">
+                <div className='products__filter'>
                     <button
-                        type="button"
-                        className="products__filter__btn"
+                        type='button'
+                        className='products__filter__btn'
                         onClick={togglePriceDropdown}
                     >
                         {priceDropdown ? (
-                            <div className="products__filter__dropdown__name">
+                            <div className='products__filter__dropdown__name'>
                                 <p>Price</p>
-                                <p className="products__filter__arrow">
+                                <p className='products__filter__arrow'>
                                     <UpArrow />
                                 </p>
                             </div>
                         ) : (
-                            <div className="products__filter__name">
+                            <div className='products__filter__name'>
                                 <p>Price</p>
-                                <p className="products__filter__arrow">
+                                <p className='products__filter__arrow'>
                                     <DownArrow />
                                 </p>
                             </div>
                         )}
                     </button>
                     {priceDropdown && (
-                        <div className="products__filter__dropdown">
-                            <ul className="products__filter__dropdown__list">
+                        <div className='products__filter__dropdown'>
+                            <ul className='products__filter__dropdown__list'>
                                 <li>
                                     <input
-                                        type="number"
+                                        type='number'
                                         onChange={searchProductByMinPrice}
-                                        placeholder="Min price..."
-                                        className="search__price__button"
-                                        min="0"
+                                        placeholder='Min price...'
+                                        className='search__price__button'
+                                        min='0'
                                     />
                                     -
                                     <input
-                                        type="number"
+                                        type='number'
                                         onChange={searchProductByMaxPrice}
-                                        placeholder="Max price..."
-                                        className="search__price__button"
-                                        min="0"
+                                        placeholder='Max price...'
+                                        className='search__price__button'
+                                        min='0'
                                     />
                                 </li>
                             </ul>
@@ -210,8 +282,8 @@ export const ProductsComponent = ({ productsToDisplay, itemsOnPage }: Props): JS
                     )}
                 </div>
             </div>
-            <div className="products__container--right">
-                <div className="products__items">
+            <div className='products__container--right'>
+                <div className='products__items'>
                     {productsForPage.length > 0 ? (
                         productsForPage.map((item) => {
                             return <ProductItem key={`page-product-${item._id}`} {...item} />;
