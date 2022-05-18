837188a8eb65e66e5c56113e6c8b2cf9bf28d279
Tomasz <przadkatomasz.alt@gmail.com>
2022-05-18 19:40:46 +0200

    fix pre hook in user model

--- api/models/user.model.ts
+++ api/models/user.model.ts
@@ -8,7 +8,7 @@ import {
   } from '@typegoose/typegoose';
   import bcrypt from 'bcrypt';
   
-  // @index({ email: 1 })
+
   @pre<User>('save', async function () {
     // Hash password if the password is new or was updated
     if (!this.isModified('password')) return;
@@ -24,11 +24,7 @@ import {
   })
   
 
-  export enum Role {
-      user = 'user',
-      admin = 'admin'
-  }
-
+  
   // Export the User class to be used as TypeScript type
   export class User {
     @prop({required:true})
@@ -43,8 +39,8 @@ import {
     @prop({ required: true, minlength: 8, maxLength: 32, select: false })
     password!: string;
   
-    @prop({ type: String, enum: Role, default: Role.user })
-    role: Role;
+    @prop({ enum: ['user', 'admin'], default: 'user' })
+    role: string;
 
     // @prop()
   
@@ -54,8 +50,11 @@ import {
     }
   }
   
+
   // Create the user model from the User class
   const userModel = getModelForClass(User);
+
+
   export default userModel;
   
   
